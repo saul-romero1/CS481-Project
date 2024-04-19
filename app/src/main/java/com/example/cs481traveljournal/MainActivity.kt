782b958b6e41304e475da6b984b9017ee6917257
joshualarson <larso084@csusm.edu>
2024-04-19 15:04:59 -0700

    Project started

--- app/src/main/java/com/example/cs481traveljournal/MainActivity.kt
+++ app/src/main/java/com/example/cs481traveljournal/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.cs481traveljournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Retrieve the content view that renders the map.
+
+        // Get the SupportMapFragment and request notification when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
+        mapFragment?.getMapAsync(this)
+
+    }
+    class MapsMarkerActivity : AppCompatActivity(), OnMapReadyCallback {
+
+        // ...
+
+        override fun onMapReady(googleMap: GoogleMap) {
+            val sydney = LatLng(-33.852, 151.211)
+            googleMap.addMarker(
+                MarkerOptions()
+                    .position(sydney)
+                    .title("Marker in Sydney")
+            )
+        }
     }
 }
+
+private fun <Fragment> Fragment?.getMapAsync(mainActivity: MainActivity) {
+
+}
+
+
+
--- app/src/main/java/com/example/cs481traveljournal/Map.kt
+++ app/src/main/java/com/example/cs481traveljournal/Map.kt
@@ -23,10 +23,7 @@ class Map : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
